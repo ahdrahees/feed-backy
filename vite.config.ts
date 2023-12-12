fea486919328c839070fa5ce8ce1aee741d69464
Rahees <ahdrahees.dev@gmail.com>
2023-12-12 15:52:36 +0530

    browser compatibility issue with dfx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,39 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		target: 'esnext'
+	},
+	server: {
+		proxy: {
+			'/api': 'http://localhost:8080'
+		}
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			},
+			// Enable esbuild polyfill plugins
+			plugins: [
+				// @ts-ignore
+				NodeModulesPolyfillPlugin(),
+				{
+					name: 'fix-node-globals-polyfill',
+					setup(build) {
+						build.onResolve({ filter: /_virtual-process-polyfill_\.js/ }, ({ path }) => ({ path }));
+					}
+				}
+			]
+		}
+	},
+	define: {
+		'process.env': {
+			// ...getDataFromFiles(['output/epr.json', 'output/sustainability.json', 'output/certificates.json']) // Add your data here
+		}
+	}
 });
